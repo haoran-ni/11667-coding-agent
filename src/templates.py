8dd3a1123ed6a709c9f3ee9b59af08a3d2495992
Zefeng Cai <caizefeng18@gmail.com>
2024-11-28 11:13:36 -0500

    Default to a more appropriate template for instruction models

--- src/templates.py
+++ src/templates.py
@@ -6,49 +6,25 @@
 # {instruction}
 #
 # ### Input:
-# {input}
-#
-# ### Response:
+# {input if input != '' else 'Not applicable'}
 # """
 #
 # def format_assistant_text(output):
-#     return f"""```python
+#     return f"""### Response:
+# ```python
 # {output}
 # ```
 # """
 
 def format_user_text(instruction, input):
-    return f"""### Instruction:
-Use the Task below and the Input given to write the Response, which is a Python programming code that can solve the following Task:
-
-### Task:
-{instruction}
-
-### Input:
-{input if input != '' else 'Not applicable'}
-"""
+    if input and input != 'Not applicable':
+        return instruction
+    else:
+        return f"""{instruction}
+{input}"""
 
 def format_assistant_text(output):
-    return f"""### Response:
-```python
+    return f"""```python
 {output}
 ```
 """
-
-# def format_user_text(instruction, input):
-#     return f"""### INSTRUCTION:
-# Using the provided TASK and INPUT, generate a Python program as the RESPONSE that effectively solves the given TASK based on the specified INPUT.
-#
-# ### TASK:
-# {instruction}
-#
-# ### INPUT:
-# {input}
-# """
-#
-# def format_assistant_text(output):
-#     return f"""### RESPONSE:
-# ```python
-# {output}
-# ```
-# """
